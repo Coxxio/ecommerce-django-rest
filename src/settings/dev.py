89889c80c6e348c527b4c7a8be4f95cefcfeb2fb
Antonio Cortez <antoniocortez1497@gmail.com>
2024-02-10 17:17:35 -0400

    feat: configure mongo plugin and conect to database

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -2,8 +2,16 @@
 from .base import *
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': env("DB_NAME"),
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': env("DB_HOST"),
+                'port': int(env("DB_PORT")),
+                'username': env("DB_USER"),
+                'password': env("DB_PASSWORD"),
+                'authSource': "admin"
+            }
+        }
     }
-}
